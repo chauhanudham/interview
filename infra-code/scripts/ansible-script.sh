70f57dfe933e734d25b4047c8e7495f4924dd625
chauhanudham <31729485+chauhanudham@users.noreply.github.com>
2020-02-03 00:38:20 +0530

    Update ansible-script.sh

--- infra-code/scripts/ansible-script.sh
+++ infra-code/scripts/ansible-script.sh
@@ -19,23 +19,13 @@ sudo useradd ansible
 sudo su - ansible
 sudo mkdir -p /home/ansible/.ssh
 cd  /home/ansible/.ssh 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN9XqbphSESDLhYcQB0TaL6L827t3xSUvaaV9Ksf+qErmHdWfDOPx91mXtqCRIFW4444/z0JUC5SabK/6TCVVaLCAzabEMOMzojG7QDwtxCgS16IjJWETyZEfZo3m/OTVNVJD/XL3n4+ACU0IvQKteLOGM+MKdiAr00jxKBDlkS3DEBzPSaVi4Er9SBMdNIl6FhseikOz9zTg2zDBl/Z+3ORmdlyuJ7H9WscMJQSpYdDjNHxWvsNBpzBUNvuGTaeq6WCNRECiWRVpu+c3jwB+1e2yDgttGitZbZeZnyV4Ffetf7iu8VFynltrOdcJgAhlSCaKFyVkwXFTJx8lQKa7l root@ip-10-0-1-24" >>  authorized_keys
+echo "" >>  authorized_keys
 
 ####### Change the file Permission 
 sudo chmod 755 /home/ansible/.ssh 
 sudo chmod 600 authorized_keys
 sudo chown -R ansible.ansible /home/ansible/.ssh
 cd  /home/ansible/
-
-####### Jenkins Setup 
-
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-sudo yum install jenkins -y
-sleep 3
-sudo systemctl start jenkins 
-sudo  systemctl enable  jenkins
-
 
 
 
